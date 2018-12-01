d3dc9ebf59ae8263f31b9c064d19628bd2534648
Richard <richard.fagot@gmail.com>
2018-12-01 13:18:36 +0100

    Display categories which will stand for top menu (biblio, actus...)

--- ridingcats/header.php
+++ ridingcats/header.php
@@ -1,3 +1,8 @@
 <?php
 wp_nav_menu( array( 'theme_location' => 'header-menu' ) );
+
+$categories = get_categories();
+foreach($categories as $cat) {
+  echo '<a href="' . get_category_link($cat->term_id) . '">' . $cat->name . '</a>';
+}
  ?>
